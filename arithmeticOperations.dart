4077554e56b6ce2007089a6928fb279b58c251f8
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-09 22:14:08 +0530

    Basic arithmetic operations in dart

---
+++ arithmeticOperations.dart
@@ -0,0 +1,13 @@
+void main() {
+int a=8;
+  int b=16;
+  print(a+b);//add
+  print(a-b);//subtract
+  print(a*b);//multiply
+  print(a/b);//divide
+  print(a~/b);//divide and give integer
+  print(a%b);//remainder
+  print(-(a-b));//unary minus
+  print(a++);//increment
+  print(b--);//decrement
+}
