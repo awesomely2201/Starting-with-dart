5148deda75ebee445735ab5cb82e956c3aaca606
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-09 22:20:10 +0530

    Equality and relational operator in dart

---
+++ operators.dart
@@ -0,0 +1,15 @@
+void main(){
+int a=10,b=20;
+  if(a<b)//less than
+  print(a);
+  if(a>b)//greater than
+  print(b);
+  if(a>=b)//greater than equal to
+  print(a);
+  if(a<=b)//less than equal to
+  print(b);
+  if(a==b)//equality
+  print(a);
+  if(a!=b)//not equal to
+  print(b);
+}
