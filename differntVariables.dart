cd9302b277f52c34ad9de2833e8a30d628120387
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-09 13:08:06 +0530

    Different types of variables
    var and dynamic are the same thing. final and const are also the same just the difference is const can also be declared at compile-time and called as a compile-time constant and whereas final cannot.

--- differntVariables.dart
+++ differntVariables.dart
@@ -3,10 +3,14 @@ void main() {
   dynamic b="abc";
   int c=2;
   bool d=true;
-  String e="hello"; 
+  String e="hello";
+  final f=1;
+  const g="world";
   print(a);
   print(b);
   print(c);
   print(d);
   print(e);
+  print(f);
+  print(g);
 }
