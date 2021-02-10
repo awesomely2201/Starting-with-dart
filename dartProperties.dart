aff3350d9528f1c4c95d23c8bd9bcc84a52a2dbd
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-10 23:42:31 +0530

    Dart number properties

---
+++ dartProperties.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 10;
+  print(a.isEven);//returns true if even
+  print(a.hashCode);//returns hashcode value of that integer
+  print(a.isFinite);//returns true if finite
+  print(a.isInfinite);//returns true if infinite
+  print(a.isOdd);//returns true if odd
+  print(a.isNaN);//returns true if not a number
+  print(a.isNegative);//returns true if negative
+}
