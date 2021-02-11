81e6ea84dedcb4fc06adcfa4b8be11288f2e4b08
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-11 23:37:28 +0530

    Update list in dart
    basic assigning new value at the point we want to change

---
+++ updateList.dart
@@ -0,0 +1,6 @@
+void main() { 
+  var even_list = [2,4,5,8,10]; 
+  print("List values before update element : ${even_list}"); 
+  even_list[2] = 6;
+  print("List values after updating list element : ${even_list}"); 
+}
