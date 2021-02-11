68d51d36405d35d9a62be13e727ba88957e66a28
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-11 23:43:15 +0530

    RemoveLast in list
    removeLast() is used to remove last element in list.

---
+++ removeLastInList.dart
@@ -0,0 +1,6 @@
+void main() { 
+  var even_list = [2,4,6,8,10]; 
+  print("List values before removing element : ${even_list}"); 
+  even_list.removeLast();
+  print("List values after removing last element : ${even_list}"); 
+}
