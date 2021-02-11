5adeb3ee4a7e6300d38a4079a8feb4e568dedcfe
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-11 23:40:02 +0530

    Remove in list
    remove() for the element index we need to remove.

---
+++ removeInList.dart
@@ -0,0 +1,6 @@
+void main() { 
+  var even_list = [2,4,6,8,10]; 
+  print("List values before removing element : ${even_list}"); 
+  even_list.remove(6);
+  print("List values after removing element : ${even_list}"); 
+}
