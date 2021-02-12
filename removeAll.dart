5786af4be7ca2f2606fd88464d30b24387155853
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-12 23:50:07 +0530

    clear() for removing all in dart.

---
+++ removeAll.dart
@@ -0,0 +1,9 @@
+void main() { 
+  var persons = <String>{"John", "Doe", "Smith", "Alex"};
+  print("Dart remove all elements from set.");
+  print("Before Clear");
+  print(persons);
+  print("After Clear");
+  persons.clear();
+  print(persons);
+}
