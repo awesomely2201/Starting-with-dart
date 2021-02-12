7360e613d37152423293079bdadee8f731d95646
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-12 23:49:07 +0530

    forEach() in dart.

---
+++ forEach.dart
@@ -0,0 +1,7 @@
+void main() { 
+  var persons = <String>{"John", "Doe", "Smith", "Alex"};
+  print("Dart Iterating Set Elements.");
+  persons.forEach((value) {
+    print('Value: $value');
+  });
+}
