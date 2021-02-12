f8f64026fd925f935e0c9d9b4b74c856bed1aead
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-12 23:44:52 +0530

    Add() in dart.

--- setsAdd.dart
+++ setsAdd.dart
@@ -1,7 +1,7 @@
 void main() { 
   var names = {"John", "Doe", "Smith", "Alex"};
   var persons = <String>{};
-  print("W3Adda - Dart insert Item(s) into Set.");
+  print("Dart insert Item(s) into Set.");
   persons.add("Murphy");
   print(persons);
   persons.addAll(names);
