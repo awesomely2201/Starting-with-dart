2fdeb70cbf331aa581c80e9ddfc71e0e75fb3ab2
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-13 23:45:07 +0530

    Map using constructor in dart.

---
+++ mapUsingConstructor.dart
@@ -0,0 +1,11 @@
+void main() { 
+  var weekDays = new Map();
+  weekDays['mon']  = "Monday";
+  weekDays['tue']  = "Tuesday";
+  weekDays['wed']  = "Wednesday";
+  weekDays['thu']  = "Thursday";
+  weekDays['fri']  = "Friday";
+  weekDays['sat']  = "Saturday";
+  weekDays['sun']  = "Sunday";
+  print(weekDays);
+}
