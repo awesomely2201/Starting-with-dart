d75b85c0cffcc4848d895248e7dc9524cef89d19
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-13 23:59:22 +0530

    if else in dart.
    Decision-making statements in the dart.

---
+++ ifElse.dart
@@ -0,0 +1,14 @@
+void main()
+{
+    var a = 10;
+    var b = 20;
+    print("Dart If else Statement");
+ 	
+    if(a > b){
+    print("a is greater than b");
+    }
+    else {
+    print("b is greater than a");
+    }
+
+}
