44b70cd888d0bbb7103172e2a9e7840e64773019
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-14 23:21:56 +0530

    do while loop in dart.
    It executes the code segment at least once.

---
+++ doWhileLoop.dart
@@ -0,0 +1,11 @@
+void main()
+{
+    var ctr =1;
+    var maxCtr =5;
+    print("Dart do while loop");
+    do{      
+        print("Hello World! Value is :${ctr}");  
+        ctr = ctr+1;
+    }while(ctr<=maxCtr);
+
+}
