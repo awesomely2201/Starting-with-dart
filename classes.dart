5019845b416e8eee08d827eeb14b5b5ab6fc0bfc
Harsh Mangla <harsh1234.cse18@chitkara.edu.in>
2021-02-16 22:20:52 +0530

    Classes in dart.
    Employee is a class and Name, age and salary are the fields inside it.

---
+++ classes.dart
@@ -0,0 +1,12 @@
+class Employee {
+   var empName;
+   var empAge;
+   var empSalary;
+  
+  showEmpInfo(){
+    print("Employee Name Is : ${empName}");
+    print("Employee Age Is : ${empAge}");
+    print("Employee Salary Is : ${empSalary}");
+  }
+  
+}
